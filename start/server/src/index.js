5d95782cc1ddb3a9ea95311d04a70d2311ed7398
Andrey_Shleger <Andrey_Shleger@epam.com>
2020-11-10 21:49:38 +0300

    add mocks

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,8 +3,12 @@
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const mocks = require('./mocks');
 
-const server = new ApolloServer({ typeDefs });
+
+
+const server = new ApolloServer({ typeDefs , resolvers, mocks});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
---
+++ start/server/src/mocks.js
@@ -0,0 +1,13 @@
+const mocks = {
+  BlockItem: () => ({ 
+    type: "ssss",
+    id: "uid",
+    caption: "cap",
+    order: 123456,
+    scrollType: "noScroll",
+    __typename: "CoreBlock"
+  }),
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => 'Hello11',
+};
